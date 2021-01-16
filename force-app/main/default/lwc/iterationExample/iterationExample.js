f2e1d6de0db5a2c79dafe18eea6b754e8a9ae5b7
Saurabh Kher <skher@in.imshealth.com>
2021-01-16 23:18:57 +0530

    Iteration Commit

---
+++ force-app/main/default/lwc/iterationExample/iterationExample.js
@@ -0,0 +1,26 @@
+import { LightningElement } from "lwc";
+
+export default class IterationExample extends LightningElement {
+  contacts = [
+    {
+      Id: 1,
+      Name: "Saurabh Kher",
+      Email: "saurabhkher19@gmail.com",
+    },
+    {
+      Id: 2,
+      Name: "Abhishek Jain",
+      Email: "abhishekjain@gmail.com",
+    },
+    {
+      Id: 3,
+      Name: "Akash Agrawal",
+      Email: "akash.agr@gmail.com",
+    },
+    {
+      Id: 4,
+      Name: "Ankit Soni",
+      Email: "ankit.soni@gmail.com",
+    },
+  ];
+}
